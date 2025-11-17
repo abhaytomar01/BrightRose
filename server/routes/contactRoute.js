31af0c83eaf38c2c05b717a8c26a1e565b6d8808
Abhay Tomar <abhaytomar440@gmail.comgit config --global user.name Abhay>
2025-11-17 16:16:15 +0530

    updated api path contact

--- server/routes/contactRoute.js
+++ server/routes/contactRoute.js
@@ -3,6 +3,6 @@ import { sendContactMessage } from "../controllers/contact/contactController.js"
 
 const router = express.Router();
 
-router.post("/contact", sendContactMessage);
+router.post("/", sendContactMessage);
 
 export default router;
