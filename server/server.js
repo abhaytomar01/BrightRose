8c0bd97096a60be8cc1be31f45a71e948a936225
Abhay Tomar <abhaytomar440@gmail.comgit config --global user.name Abhay>
2025-11-27 12:52:04 +0530

    Update limit sze

--- server/server.js
+++ server/server.js
@@ -68,9 +68,9 @@ app.use(
 // ==============================
 // Body Parser
 // ==============================
-app.use(express.json({ limit: "50mb" }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "300mb" }));
+app.use(bodyParser.json({ limit: "300mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "300mb" }));
 
 // ==============================
 // Logger
