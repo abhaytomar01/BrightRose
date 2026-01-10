66959262bc1678e455af3f4fd561aef59bc35523
Abhay Tomar <abhaytomar440@gmail.comgit config --global user.name Abhay>
2026-01-10 21:15:23 +0530

    updte admin path

--- client/src/pages/Admin/AdminLogin.jsx
+++ client/src/pages/Admin/AdminLogin.jsx
@@ -49,7 +49,7 @@ const AdminLogin = () => {
     setSubmitting(true);
     try {
       // Use api client directly (baseURL + interceptors)
-      const res = await api.post("/api/v1/auth/admin-login", { email, password });
+      const res = await api.post("/auth/admin-login", { email, password });
 
 
       if (res?.data?.success) {
