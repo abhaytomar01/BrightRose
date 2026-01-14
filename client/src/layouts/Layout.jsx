0a77db259082d2f6b9bc69557a0e7f259d35cc5c
Abhay Tomar <abhaytomar440@gmail.comgit config --global user.name Abhay>
2026-01-14 15:36:19 +0530

    update cart

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import Header from "./../components/header/Header";
 import Footer from "./../components/footer/Footer";
 import Routers from "../routes/Routers";
-import WhatsAppButton from "../components/WhatsAppButton.jsx";
+import WhatsAppButton from "../context/WhatsAppButton.jsx";
 // import SmoothScroll from "../components/SmoothScroll";
 const Layout = () => {
     return (
